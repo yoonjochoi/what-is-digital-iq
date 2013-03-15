208d8a8a7e3a5fc2fc42699d5ee3425eb49b67e0
L2 Think Tank <Elyte@Sarinas-iMac.local>
2013-03-15 14:56:25 -0400

    pushing code

--- static/js/script.js
+++ static/js/script.js
@@ -1,27 +1,111 @@
 init = function(){
 	var jarallax = new Jarallax();
-	jarallax.addAnimation('#yoonjoIs', 
-						   [{progress:'0%', top: '100%'},
-						   {progress:'10%', top: '60%'},
-						   {progress:'100%', top: '60%'}]);
-	
-	jarallax.addAnimation('#topBanner', 
-						   [{progress:'0%', top: '100%'},
-						   {progress:'20%', top: '100%'},
-						   {progress:'40%', top: '0%'},
-						   {progress:'100%', top: '0%'}]);
+	jarallax.addAnimation('#l2-logo', 
+						   [{progress:'0%', top: '50%', opacity: '1'},
+						   {progress:'10%', top: '10%', opacity: '1'},
+						   {progress:'15%', top: '5%', opacity: '0',},
+						   {progress:'75%', top: '5%', opacity: '0'}]);
 						   
-	jarallax.addAnimation('#thumbnailsContainer', 
-						   [{progress:'0%', top: '100%'},
-						   {progress:'20%', top: '100%'},
-						   {progress:'40%', top: '0%'},
-						   {progress:'100%', top: '0%'}]);
-	
-	jarallax.addAnimation('#bottom', 
-						   [{progress:'0%', top: '100%'},
-						   {progress:'80%', top: '100%'},
-						   {progress:'100%', top: '0%'}]);
+	jarallax.addAnimation('h1', 
+						   [{progress:'0%', top: '50%', opacity: '1'},
+						   {progress:'10%', top: '10%', opacity: '1'},
+						   {progress:'15%', top: '5%', opacity: '0.5'},
+						   {progress:'75%', top: '5%', opacity: '0.5'}]);
 
+
+
+	jarallax.addAnimation('#intro-page', 
+						   [{progress:'0%', top: '100%'},
+						   {progress:'10%', top: '30%', opacity: '1'},
+						   {progress:'15%', top: '25%', opacity: '0.5'},
+						   {progress:'75%', top: '25%', opacity: '0.5'}]);
+						   
+	jarallax.addAnimation('#intro-page-site', 
+						   [{progress:'0%', top: '100%'},
+						   {progress:'10%', top: '50%', opacity: '1'},
+						   {progress:'15%', top: '45%', opacity: '0.5'},
+						   {progress:'75%', top: '45%', opacity: '0'}]);
+						   
+	jarallax.addAnimation('#intro-page-digital-marketing', 
+						   [{progress:'0%', top: '100%'},
+						   {progress:'10%', top: '50%', opacity: '1'},
+						   {progress:'15%', top: '45%', opacity: '0.5'},
+						   {progress:'75%', top: '45%', opacity: '0'}]);
+
+	jarallax.addAnimation('#intro-page-mobile', 
+						   [{progress:'0%', top: '100%'},
+						   {progress:'10%', top: '50%', opacity: '1'},
+						   {progress:'15%', top: '45%', opacity: '0.5'},
+						   {progress:'75%', top: '45%', opacity: '0'}]);
+
+	jarallax.addAnimation('#intro-page-social-media', 
+						   [{progress:'0%', top: '100%'},
+						   {progress:'10%', top: '50%', opacity: '1'},
+						   {progress:'15%', top: '45%', opacity: '0.5'},
+						   {progress:'75%', top: '45%', opacity: '0'}]);
+						   						   	
+
+
+	jarallax.addAnimation('#site', 
+						   [{progress:'0%', top: '100%', opacity: '1'},
+						   {progress:'30%', top: '100%', opacity: '1'},
+						   {progress:'45%', top: '36%', opacity: '1'},
+						   {progress:'55%', top: '36%', opacity: '0'},
+						   {progress:'100%', top: '36%', opacity: '0'}]);
+						   
+	jarallax.addAnimation('#digitalMarketing', 
+						   [{progress:'0%', top: '100%', opacity: '1'},
+						   {progress:'45%', top: '100%', opacity: '1'},
+						   {progress:'60%', top: '36%', opacity: '1'},
+						   {progress:'70%', top: '36%', opacity: '0'},
+						   {progress:'100%', top: '36%', opacity: '0'}]);
+						   
+	jarallax.addAnimation('#mobile', 
+						   [{progress:'0%', top: '100%', opacity: '1'},
+						   {progress:'60%', top: '100%', opacity: '1'},
+						   {progress:'75%', top: '36%', opacity: '1'},
+						   {progress:'85%', top: '36%', opacity: '0'},
+						   {progress:'100%', top: '36%', opacity: '0'}]);					   
+	
+	jarallax.addAnimation('#socialMedia', 
+						   [{progress:'0%', top: '100%', opacity: '1'},
+						   {progress:'75%', top: '100%', opacity: '1'},
+						   {progress:'90%', top: '36%', opacity: '1'},
+						   {progress:'95%', top: '36%', opacity: '0'},
+						   {progress:'100%', top: '36%', opacity: '0'}]);
+						   
+						   
+						   
+	jarallax.addAnimation('#bottom-icon-site', 
+						   [{progress:'0%', top: '100%'},
+						   {progress:'30%', top: '100%'},
+						   {progress:'45%', top: '85%'},
+						   {progress:'100%', top: '85%'}]);
+						   
+	jarallax.addAnimation('#bottom-icon-digital-marketing', 
+						   [{progress:'0%', top: '100%'},
+						   {progress:'30%', top: '100%'},
+						   {progress:'45%', top: '85%'},
+						   {progress:'100%', top: '85%'}]);
+						
+	jarallax.addAnimation('#bottom-icon-Mobile', 
+						   [{progress:'0%', top: '100%'},
+						   {progress:'30%', top: '100%'},
+						   {progress:'45%', top: '85%'},
+						   {progress:'100%', top: '85%'}]);
+						   
+	jarallax.addAnimation('#bottom-icon-social-media', 
+						   [{progress:'0%', top: '100%'},
+						   {progress:'30%', top: '100%'},
+						   {progress:'45%', top: '85%'},
+						   {progress:'100%', top: '85%'}]);
+
+
+
+	jarallax.addAnimation('#ranking', 
+						   [{progress:'0%', top: '100%'},
+						   {progress:'90%', top: '100%'},
+						   {progress:'100%', top: '0%'}]);
 
 }
 
