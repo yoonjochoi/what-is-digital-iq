c4f3aaac62e495316b99c63d8ef3f548a9c5507e
Yoonjo Choi <yoonjochoi@172-29-16-90.dynapool.nyu.edu>
2013-03-29 17:46:37 -0400

    inital commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/Elyte/Desktop/what-is-the-digital-iq-web/venv"
+set -gx VIRTUAL_ENV "/Users/yoonjochoi/Desktop/what-is-the-digital-iq-web/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
